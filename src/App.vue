2098aca56029ca2f50e49a90b8efe2143ba9b787
@douglasabnovato <douglasabnovato@gmail.com>
2020-03-22 22:21:25 -0300

    estrutura de arquivo vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Conversor de Moedas</h1>
   </div>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
-  components: {}
-}
+  export default {
+    name: 'App',
+    components: {}
+  }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
 </style>
