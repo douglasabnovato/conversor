2e2ecbe3dc3d28e5b85b088a0e62dd12a9cc829e
@douglasabnovato <douglasabnovato@gmail.com>
2020-03-23 19:17:22 -0300

    converter api

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <Conversor moedaA="USD" moedaB="BRL"/>
+    <h3 class="text-center">Conversor de Moeda</h3>
+    <div class="linha">
+      <Conversor moedaA="USD" moedaB="BRL"/>
+      <Conversor moedaA="BRL" moedaB="USD"/>
+    </div>   
+    <div class="linha">
+      <Conversor moedaA="CAD" moedaB="BRL"/>
+      <Conversor moedaA="BRL" moedaB="CAD"/>
+    </div> 
+    <div class="linha">
+      <Conversor moedaA="EUR" moedaB="BRL"/>
+      <Conversor moedaA="BRL" moedaB="EUR"/>
+    </div>  
+    <h5 class="text-footer">@douglasabnovato</h5>    
   </div>
 </template>
 
@@ -18,12 +31,27 @@
 <style>
 
   #app {
+    margin: auto;
+    max-width: 900px;
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+    -moz-osx-font-smoothing: grayscale;    
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+  .linha{
+    margin-top: 40px;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .text-center{
+    text-align: center;
+  }
+
+  .text-footer{
+    text-align: right;
   }
 
 </style>
--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -3,7 +3,7 @@
     <div class="conversor">
         <h2>{{moedaA}} para {{moedaB}}</h2>
         <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
-        <input type="button" value="Converter">
+        <input type="button" value="Converter" v-on:click="converter">
         <h2>{{moedaB_value}}</h2>
     </div>  
 
@@ -19,19 +19,34 @@
                 moedaA_value : "",
                 moedaB_value : 0
             }
-        },
+        }/*,
         methods:{
             converter(){
-                let de_para = this.moedaA + _ + this.moedaB
-                let url = "https://free.currconv.com/api/v7/convert?q="
-                         + de_para
-                         +"&compact=ultra&apiKey=4cb0cbd6ee5ca563ce41";                                        
+                let de_para = this.moedaA + "_" + this.moedaB;
+
+                let url = 
+                    "https://free.currconv.com/api/v7/convert?q="
+                    + de_para
+                    +"L&compact=ultra&apiKey=4cb0cbd6ee5ca563ce41";       
+
+            fetch(url)
+                .then(res => {
+                    return res.json();
+                })
+                .then(json => { 
+                    let cotacao = json[de_para].val;
+                    this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
+                });
             }
-        }
+        }*/
     };
 
 </script>
 
 <style scoped>
-
+    .conversor{
+        padding: 20px;
+        max-width:300px;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,.2);    
+    }
 </style>
