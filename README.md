96bde3983786a8d048de44f017321de41a1dae52
@douglasabnovato <douglasabnovato@gmail.com>
2020-03-23 20:14:39 -0300

    configurar api de converter

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Conversor - Vuejs
-- API : `https://free.currencyconverterapi.com/`
-- Dolar e Real: {"USD_BRL":5.0539}
+- API de conversão: `https://free.currencyconverterapi.com/`
+- Arquivos: `index.html`, `main.js`, `App.vue`, `Conversor.vue`
+
 
 de : [Programador BR](https://www.youtube.com/watch?v=tIEa3MRBpI0)
 
--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -18,27 +18,29 @@
             return{
                 moedaA_value : "",
                 moedaB_value : 0
-            }
-        }/*,
+            };
+        },
         methods:{
+
             converter(){
                 let de_para = this.moedaA + "_" + this.moedaB;
 
                 let url = 
                     "https://free.currconv.com/api/v7/convert?q="
                     + de_para
-                    +"L&compact=ultra&apiKey=4cb0cbd6ee5ca563ce41";       
-
-            fetch(url)
-                .then(res => {
-                    return res.json();
-                })
-                .then(json => { 
-                    let cotacao = json[de_para].val;
-                    this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
-                });
+                    + "&compact=ultra&apiKey=4cb0cbd6ee5ca563ce41";
+                          
+            
+                fetch(url)
+                    .then(res => {
+                        return res.json();
+                    })
+                    .then(json => { 
+                        let cotacao = json[de_para].val;
+                        this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
+                    });
             }
-        }*/
+        }
     };
 
 </script>
